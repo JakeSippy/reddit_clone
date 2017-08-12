b286c84c38c5cf217d54bb4013e03b8e4dd6769a
Jake Sippy <mrjsippy@gmail.com>
2017-08-11 18:54:49 -0700

    Fixed port issue

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 express = require('express');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.get('/', function(req, res) {
     res.send('Hello');
 });
 
-app.listen(port, function() {
+app.listen((process.env.PORT || port), function() {
     console.log(`Listening at http://127.0.0.1:${port}`);
 });
