6adf4391e78629ee1686508dfaff3756ec874396
Jake Sippy <mrjsippy@gmail.com>
2017-08-14 00:11:20 -0700

    removed test files

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const { Pool, Client } = require('pg');
 const app = express();
 const port = 5000;
 
+console.log("URL HERE: " + process.env);
+
+
 app.get('/', function(req, res) {
     res.send('Hello');
-    res.send('Process.env: ' + process.env);
 });
 
 app.listen((process.env.PORT || port), function() {
     console.log(`Listening at http://127.0.0.1:${port}`);
-    console.log(process.env);
 });
