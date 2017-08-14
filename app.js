ca7685a6f417c93da8446a56f2f4576fc6e11d5b
Jake Sippy <mrjsippy@gmail.com>
2017-08-14 01:06:12 -0700

    added gitignore and .env files

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const { Pool, Client } = require('pg');
 const app = express();
 const port = 5000;
 
-console.log("URL HERE: " + process.env);
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
 
+pool.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    console.log(err, res);
+    pool.end();
+});
 
 app.get('/', function(req, res) {
     res.send('Hello');
