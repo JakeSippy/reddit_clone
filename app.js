18e2d12eaf0d41f2c2a56b4e685243737d94228b
Jake Sippy <mrjsippy@gmail.com>
2017-08-13 21:39:18 -0700

    first attempt to add postgreSQL to app

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 express = require('express');
+var pg = require('pg');
 
 const app = express();
 const port = 5000;
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, fuction() {
+    if (err) throw err;
+    console.log('Connected to postgrs! Getting schemas...');
+
+    client
+        .query('SELECT table_schemma,table_name FROM information_schema.tables;')
+        .on('row', fuction(row) {
+            console.log(JSON.stringify(row));
+        });
+});
+
 
 app.get('/', function(req, res) {
     res.send('Hello');
