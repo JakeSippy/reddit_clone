9b5ab7f2790c9e6a98546f952abd2ca879ad00e7
Jake Sippy <mrjsippy@gmail.com>
2017-08-13 23:47:46 -0700

    added sketchy fix

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const port = 5000;
 
 app.get('/', function(req, res) {
     res.send('Hello');
-    res.send(process.env.DATABASE_URL);
 });
 
 app.listen((process.env.PORT || port), function() {
     console.log(`Listening at http://127.0.0.1:${port}`);
+    console.log(process.env);
 });
