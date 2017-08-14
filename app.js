a7327f177113d133405873f29366cf729656dc24
Jake Sippy <mrjsippy@gmail.com>
2017-08-13 23:43:16 -0700

    didnt do much

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 express = require('express');
-var pg = require('pg');
+const { Pool, Client } = require('pg');
 
 const app = express();
 const port = 5000;
 
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, fuction() {
-    if (err) throw err;
-    console.log('Connected to postgrs! Getting schemas...');
-
-    client
-        .query('SELECT table_schemma,table_name FROM information_schema.tables;')
-        .on('row', fuction(row) {
-            console.log(JSON.stringify(row));
-        });
-});
-
-
 app.get('/', function(req, res) {
     res.send('Hello');
+    res.send(process.env.DATABASE_URL);
 });
 
 app.listen((process.env.PORT || port), function() {
