d2871f5c938f8911219ab9998c9337d735495c3b
Jake Sippy <mrjsippy@gmail.com>
2017-08-16 23:07:50 -0700

    Got posts to display on the index page

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
 var express = require('express');
+const db = require('./db/index.js');
 var app = express();
 
 // port to be used for local deployment
 const port = 5000;
 
-// set EJS as the view engine and prettify output
+// set EJS as the view engine
 app.set('view engine', 'ejs');
-app.locals.pretty = true;
 
 // Home page
 app.get('/', function(req, res) {
-    res.render('pages/index.ejs');
+    posts = db.query('SELECT * FROM postdata;', [], function(err, res) {
+        if (err) {
+            console.log(err, res);
+        }
+        res.render('pages/index.ejs', {posts: res});
+    });
 });
 
+// About page (move to routing dir later)
 app.get('/about', function(req, res) {
     res.render('pages/about.ejs');
 });
 
-// Routing for static files (images, etc.)
+// Data page for testing
+app.get('/data', function(req, res) {
+    db.query('SELECT * FROM postdata', [], (err, result) => {
+        if (err) {
+            return err;
+        }
+        res.send(result);
+    });
+});
+    
+
+// Routing for static files
 app.use('/static', express.static(__dirname + 'public'));
 
 // Start listening for connections
